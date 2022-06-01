6bb02f0d0ab5a9bf7339e856033254bab96cef74
ArBe <argon-beryllium@protonmail.com>
2022-06-01 21:53:36 +0200

    made Ball store the inverse mass, to spare some expensive divisions on every frame

--- src/ball.h
+++ src/ball.h
@@ -6,10 +6,12 @@ struct Ball
 	v2f pos, p_pos;
 	v2f acc;
 	float r;
-	float mass;
+	float inv_mass;
+	inline float getMass() const { return 1/inv_mass; }
+	inline void setMass(float m) { inv_mass = m?1/m:0; }
 
 	Ball(const v2f& pos_ = {0,0}, float r_ = 1, float mass_ = 1, const v2f& iv = {0,0}) :
-		pos(pos_), p_pos(pos-iv), r(r_), mass(mass_) {}
+		pos(pos_), p_pos(pos-iv), r(r_), inv_mass(mass_?1/mass_:0) {}
 
 	v2f getVel() const;
 
