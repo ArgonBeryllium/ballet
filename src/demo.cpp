b13f29c1fe3434c2feeea0171839b1ee38dc64e7
ArBe <argon-beryllium@protonmail.com>
2022-07-03 20:37:13 +0200

    edge constraint prototype

--- src/demo.cpp
+++ src/demo.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "effectors.h"
 #include "joints.h"
 #include "shitrndr/src/shitrndr.h"
 
@@ -17,23 +18,63 @@ v2f CP = {0,0};
 inline vec2<int> toScreen(const v2f& p)
 {
 	using namespace shitrndr;
-	int xo = WindowProps::getWidth()/2 - CP.x*PS;
-	int yo = WindowProps::getHeight()/2 - CP.y*PS;
+	int xo = WindowProps::getWidth()/2. - CP.x*PS;
+	int yo = WindowProps::getHeight()/2. - CP.y*PS;
 	return (p*PS+vec2<int>(xo, yo)).to<int>();
 }
 inline v2f toSpace(const vec2<int>& p)
 {
 	using namespace shitrndr;
-	int xo = WindowProps::getWidth()/2 - CP.x*PS;
-	int yo = WindowProps::getHeight()/2 - CP.y*PS;
+	int xo = WindowProps::getWidth()/2. - CP.x*PS;
+	int yo = WindowProps::getHeight()/2. - CP.y*PS;
 	return (p-vec2<int>(xo, yo)).to<float>()/PS;
 }
+
+struct Edge
+{
+	v2f a, b;
+	float r = 0;
+	v2f dif() const { return b-a; }
+
+	void collide(Ball* ball)
+	{
+		v2f ba = dif();
+		v2f pa = ball->pos-a;
+		float h = v2f::dot(pa,ba)/v2f::dot(ba,ba);
+		h = std::min(1.f, std::max(0.f, h));
+		v2f disp = pa - h*ba;
+		float dl = disp.getLength() - r;
+
+		if(dl >= ball->r) return;
+
+		ball->pos -= disp-(disp/dl * ball->r);
+	}
+};
+struct EffEdge : Effector
+{
+	std::vector<Edge> edges;
+	void apply(std::vector<Ball*>& balls, float d) override
+	{
+		for(auto e : edges)
+			for(auto b : balls)
+				e.collide(b);
+	}
+};
 
 int main()
 {
 	World world;
 	world.effectors.push_back(new EffGravity);
 	world.effectors.push_back(new EffCollision);
+	auto ee = new EffEdge;
+	world.effectors.push_back(ee);
+	ee->edges.push_back({{-3, 27}, {3, 27}, 2});
+	constexpr float a = 32, t = 4;
+	ee->edges.push_back({{-a,-a}, {a,-a}, t});
+	ee->edges.push_back({{-a,a}, {a,a}, t});
+	ee->edges.push_back({{-a,-a}, {-a,a}, t});
+	ee->edges.push_back({{a,-a}, {a,a}, t});
+	Edge& e = ee->edges[0];
 
 	for(int i = 0; i != 12; i++)
 	{
@@ -46,15 +87,28 @@ int main()
 	init("ballet demo", 480, 480, 1, 0);
 	silentDefs();
 	bg_col = {5,8,15,255};
-	onRender = [&world](double d, double t)
+	onRender = [&world, &e, ee](double d, double t)
 	{
 		v2f mp = toSpace(Input::getMP());
-		world.balls[0]->teleport({std::sin(world.time), -2});
-		if(Input::getMB(1))
-			world.balls[world.balls.size()-1]->teleport(mp);
-			//world.balls[0]->accelerate((mp-world.balls[0]->pos).normalised()*1000);
+		//world.balls[0]->teleport({std::sin(world.time), -2});
+		if(!Input::getKey(SDLK_LSHIFT))
+		{
+			if(Input::getMB(1))
+				world.balls[world.balls.size()-1]->teleport(mp);
+				//world.balls[0]->accelerate((mp-world.balls[0]->pos).normalised()*1000);
+			if(Input::getMB(3))
+				world.balls[0]->teleport(mp);
+		}
+		else
+		{
+			if(Input::getMB(1)) e.a = mp;
+			if(Input::getMB(3)) e.b = mp;
+		}
+		if(Input::getKey(SDLK_r))
+			for(auto b : world.balls)
+				b->p_pos = b->pos;
 
-		world.update(FIX_PACE?FIXED_STEP:d, 1);
+		world.update(FIX_PACE?FIXED_STEP:d, 4);
 
 		SetColour({255,150,150,255});
 		for(auto b : world.balls)
@@ -79,6 +133,21 @@ int main()
 
 			SetColour({255,255,0,150});
 			SDL_RenderDrawLine(ren, pa.x, pa.y, pb.x, pb.y);
+		}
+		for(auto e_ : ee->edges)
+		{
+			auto ea = toScreen(e_.a);
+			auto eb = toScreen(e_.b);
+			SetColour({255,255,255,255});
+			DrawCircle(ea.x, ea.y, e_.r*PS);
+			DrawCircle(eb.x, eb.y, e_.r*PS);
+			DrawLine(ea.x, ea.y, eb.x, eb.y);
+			auto o = e_.a-e_.b;
+			o = o.normalised();
+			o = {-o.y, o.x};
+			o = o*e_.r*PS;
+			DrawLine(ea.x+o.x, ea.y+o.y, eb.x+o.x, eb.y+o.y);
+			DrawLine(ea.x-o.x, ea.y-o.y, eb.x-o.x, eb.y-o.y);
 		}
 
 		if(FIX_PACE && d < FIXED_STEP)
